4dbff8ce6208728e6748d0fc01669ef8d944f8f2
Gitesh Shah <gjshah7254@gmail.com>
2015-06-12 14:23:26 +1200

    MVC project Added

--- MVC/WorkflowMax/WorkflowMax/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WorkflowMax.MvcApplication" Language="C#" %>
---
+++ MVC/WorkflowMaxMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WorkflowMaxMVC.Startup))]
+namespace WorkflowMaxMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
